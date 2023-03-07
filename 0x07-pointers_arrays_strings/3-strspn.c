39642485c34718132f99175b87cee2f0b0b288b9
Mike <mjayivi1@gmail.com>
2023-03-07 05:29:31 -0800

    Search a string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * _strspn - search a string for a set of bytes
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int z = 0, y, t = 0;
+
+	while (accept[z])
+	{
+		b = 0;
+
+		while (s[y] != 32)
+		{
+			if (accept[z] == s[y])
+			{
+				t++;
+			}
+
+			y++;
+		}
+
+		z++;
+	}
+
+	return (t);
+}
