87c18460a54af0174a855af1afef11fec4334ff1
Mike <mjayivi1@gmail.com>
2023-03-21 13:18:04 -0700

    Dog fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: The attributes of a dog.
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+#endif
